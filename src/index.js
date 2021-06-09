6c769ae61d387f23ab484afb290e07be58e6ade2
HwangSunTae <pozafly@gmail.com>
2021-06-09 17:09:48 +0900

    [ADD] app.js

--- src/index.js
+++
@@ -1,4 +0,0 @@
-const a = 1;
-
-console.log(a);
-console.log(1);
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'none',
-  entry: './src/index.js',
+  entry: './src/app.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
